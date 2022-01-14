104b101918c8d660de10f13a2db0e59ebfc45b05
t0a5ted <flameburstunstoppable@gmail.com>
2022-01-14 13:50:29 +0800

    Rename tokenizer file

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,43 @@
 use rustyline::Editor;
-mod tokenizer;
+mod lexer;
 
 fn main() {
 	let mut rl = Editor::<()>::new();
 	let user_in = rl.readline("Input: ").unwrap();
 
-	let toks: Vec::<tokenizer::Token> = tokenizer::tokenize(&user_in);
+	let toks: Vec::<lexer::Token> = lexer::tokenize(&user_in);
 	println!("Tokens: {:?}", toks);
 	
-	let rpn_toks: Vec::<&tokenizer::Token> = shunting_yard(&toks);
+	let rpn_toks: Vec::<&lexer::Token> = shunting_yard(&toks);
 	print!("\nRPN: ");
 	for tok in rpn_toks {
 		print!("{} ", tok.val);
 	}
 }
 
-fn shunting_yard(toks: &Vec::<tokenizer::Token>) -> Vec::<&tokenizer::Token>{
-	let mut out_stack: Vec::<&tokenizer::Token> = Vec::new();
-	let mut op_stack: Vec::<&tokenizer::Token> = Vec::new();
+fn shunting_yard(toks: &Vec::<lexer::Token>) -> Vec::<&lexer::Token>{
+	let mut out_stack: Vec::<&lexer::Token> = Vec::new();
+	let mut op_stack: Vec::<&lexer::Token> = Vec::new();
 
 	for tok in toks.iter() {
-		if tok.variant == tokenizer::TokenType::OpenParen {
+		if tok.variant == lexer::TokenType::OpenParen {
 				op_stack.push(tok);
-		} else if tok.variant == tokenizer::TokenType::CloseParen {
+		} else if tok.variant == lexer::TokenType::CloseParen {
 			while op_stack.last().expect("Mismatched Parenthesis!").val != "(" {
 				out_stack.push(op_stack.pop().unwrap());
 			}
 			op_stack.pop().unwrap();
 		}
-		else if tok.variant == tokenizer::TokenType::Op {
+		else if tok.variant == lexer::TokenType::Op {
 			// println!("{:?}, {:?}", tok, op_stack.last().unwrap());
 			while op_stack.len() > 0 &&
-				op_stack.last().unwrap().variant != tokenizer::TokenType::OpenParen &&
+				op_stack.last().unwrap().variant != lexer::TokenType::OpenParen &&
 			 	op_stack.last().unwrap().precedent.expect("Tok has no Precedence!") >= tok.precedent.expect("Tok has no Precedence!") {
 				out_stack.push(op_stack.pop().unwrap());
 			}
 			op_stack.push(tok);
 		}
-		else if tok.variant == tokenizer::TokenType::Num {
+		else if tok.variant == lexer::TokenType::Num {
 			out_stack.push(tok);
 		}
 	}
